b53b96b0dcac2b41477a23bf60cc4d5642c999cd
rishijain2002 <rishijain0652@gmail.com>
2024-09-14 13:03:26 +0530

    fetch post api

---
+++ app/api/wordpressPost/route.ts
@@ -0,0 +1,22 @@
+
+import { NextResponse } from 'next/server';
+
+
+export async function GET(request: Request) {
+  try {
+    const res = await fetch('https://blog.getgrahak.co/wp-json/wp/v2/posts?per_page=5');
+    
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    
+
+      const post = await res.json();
+
+      return new NextResponse(JSON.stringify(post));
+   
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    return new Response('Error fetching posts: ' + (error as Error).message, { status: 500 });
+  }
+}
